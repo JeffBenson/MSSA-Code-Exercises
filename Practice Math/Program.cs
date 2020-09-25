ca8f79999868fe4e94c2131246bb9072a19488b0
JeffBenson <68038932+JeffBenson@users.noreply.github.com>
2020-09-24 18:31:57 -0700

    Add files via upload

---
+++ Practice Math/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Practice_Math
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Radius Value: ");
+            float r = float.Parse(Console.ReadLine());
+
+            float d = 2 * r;
+
+            Console.WriteLine("The diameter is " + d);
+        }
+    }
+}
---
+++ Practice Quadratic Normal Form/Quadratic Normal Form/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Numerics;
+
+namespace Quadratic_Normal_Form
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Good Morning, what is your name?");
+            var a = Console.ReadLine();
+            Console.WriteLine("Hello " + a + "!");
+        }
+    }
+}
